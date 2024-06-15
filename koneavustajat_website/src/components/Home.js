2fb8174f2b3b6c8876f6b548c641899d01eb171e
Mangic-man4 <116964023+Mangic-man4@users.noreply.github.com>
2024-06-16 02:25:41 +0300

    text, button, color, counter
    added text whose color can be changed with the button. Each time the button is pressed the counter increases by one (1)

--- koneavustajat_website/src/components/Home.js
+++ koneavustajat_website/src/components/Home.js
@@ -1,7 +1,26 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Home = () => {
+  // State hooks for color and counter
+  const [color, setColor] = useState('black');
+  const [counter, setCounter] = useState(0);
+
+  // Array of colors
+  const colors = ['red', 'blue', 'green', 'purple', 'orange'];
+
+  // Function to change color and update counter
+  const changeColor = () => {
+    const newColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(newColor);
+    setCounter(prevCounter => prevCounter + 1);
+  };
+
+  // useEffect to log counter value 
+  useEffect(() => {
+    console.log(`The text has been updated ${counter} times`);
+  }, [counter]);
+
   return (
     <Container className="my-5">
       <Row className="align-items-center">
@@ -11,6 +30,9 @@ const Home = () => {
           <p>
 	  		This is the home page for KoneAvustajat
           </p>
+          <p style={{ color: color }}> This is a test! </p>
+          <Button onClick={changeColor} variant="primary">Change Color</Button>
+          <p>Color change counter: {counter}</p>
         </Col>
       </Row>
     </Container>
