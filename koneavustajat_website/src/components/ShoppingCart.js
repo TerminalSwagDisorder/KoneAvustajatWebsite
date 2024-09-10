dc5be0e59f94e4e160ca5fabba3a0f4fd2818d0a
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2024-09-10 17:11:40 +0300

    Added sorting and rendering for nested objects to cart

--- koneavustajat_website/src/components/ShoppingCart.js
+++ koneavustajat_website/src/components/ShoppingCart.js
@@ -12,6 +12,26 @@ const ShoppingCart = () => {
 	const shoppingCart = useSelector((state) => state.shoppingCart.shoppingCart);
 	const dispatch = useDispatch();
 	const cartItems = Object.values(shoppingCart);
+	const cartEntries = Object.entries(shoppingCart);
+
+
+	const sortingId = (key) => parseInt(key.split('_')[1], 10);
+	const usedPartsItems = cartEntries
+		.filter(([partKey, partVal]) => partVal.table === "usedParts");
+	const otherItems = cartEntries
+		.filter(([partKey, partVal]) => partVal.table !== "usedParts");
+
+	const orderedItems = otherItems.sort(([keyA, partValA], [keyB, partValB]) => {
+		if (partValA.table === partValB.table) {
+			return sortingId(keyA) - sortingId(keyB);
+		} else {
+			return partValA.table.localeCompare(partValB.table);
+		}
+	});
+
+	const sortedItems = [...usedPartsItems, ...orderedItems];
+	console.log(orderedItems);
+
 
 	const handleAddToCart = (item) => {
 		const newItem = {
@@ -33,12 +53,33 @@ const ShoppingCart = () => {
 	const handleClearCart = () => {
 		dispatch(clearShoppingCart());
 	};
+	
+
+
+	const renderNestedObject = (nestedObj) => {
+		return (
+			<ListGroup>
+				{Object.entries(nestedObj).map(([key, value], idx) => (
+					<ListGroup.Item key={idx}>
+						<span>
+							<b>{key}</b>:{" "}
+						</span>
+					{typeof value === "object" && value !== null ? (
+						renderNestedObject(value)
+					) : (
+						value
+					)}
+					</ListGroup.Item>
+				))}
+			</ListGroup>
+		);
+	};
 
 	const renderShoppingCartItems = () => {
-		if (shoppingCart) {
+		if (sortedItems) {
 			return (
 				<ListGroup className="parts-details">
-					{Object.entries(shoppingCart).map(([partKey, partVal]) => (
+					{sortedItems.map(([partKey, partVal]) => (
 						<ListGroup key={partKey} className="mb-4">
 							{Object.keys(partVal).map((key, idx) => (
 								<ListGroup.Item key={idx}>
@@ -56,13 +97,18 @@ const ShoppingCart = () => {
 										<a href={partVal[key]} target="_blank" rel="noopener noreferrer">
 											{partVal[key]}
 										</a>
+									) : typeof partVal[key] === "object" && partVal[key] !== null ? (
+										renderNestedObject(partVal[key])
 									) : (
 										partVal[key]
 									)}
 								</ListGroup.Item>
 							))}
 							<Button className="user-select-button" onClick={() => handleRemoveOneFromCart(partKey)}>
-								Remove 1 from cart {partKey}
+								Remove 1 {partKey} from cart
+							</Button>
+							<Button className="user-select-button" onClick={() => handleRemoveFromCart(partKey)}>
+								Remove all {partKey} from cart
 							</Button>
 						</ListGroup>
 					))}
@@ -72,6 +118,7 @@ const ShoppingCart = () => {
 			return <p>Something went wrong!</p>;
 		}
 	};
+
 
 	return (
 		<div>
