43b6f5123b69a484a53b7aeaa8f74f18aa2f3fac
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2024-10-21 00:15:02 +0300

    Changed env gen to use non-static db name

--- koneavustajat_website/env_generator.js
+++ koneavustajat_website/env_generator.js
@@ -14,20 +14,21 @@ const newJWT = crypto.randomBytes(64).toString("hex");
 const secret = `SESSION_SECRET=${newSecret}`;
 const JWT_secret = `JWT_SECRET=${newJWT}`;
 const db_host = "DB_HOST=localhost";
-const db_name = "DB_NAME=koneavustajat_db";
 const opensearch = "OPENSEARCH_URL=http://localhost:9200";
 
 rl.question("Enter DB username: ", (db_user) => {
     rl.question("Enter DB password: ", (db_password) => {
-        const env_data = `${secret}\n${JWT_secret}\n${db_host}\nDB_USER=${db_user}\nDB_PASSWORD=${db_password}\n${db_name}\n${opensearch}\n`;
+        rl.question("Enter DB name: ", (db_name) => {
+            const env_data = `${secret}\n${JWT_secret}\n${db_host}\nDB_USER=${db_user}\nDB_PASSWORD=${db_password}\nDB_NAME=${db_name}\n${opensearch}\n`;
 
-        fs.writeFile("./.env", env_data, err => {
-            if (err) {
-                console.error("Error generating env file: ", err);
-            } else {
-                console.log("Env file successfully generated");
-            }
-            rl.close();
+            fs.writeFile("./.env", env_data, (err) => {
+                if (err) {
+                    console.error("Error generating env file: ", err);
+                } else {
+                    console.log("Env file successfully generated");
+                }
+                rl.close();
+            });
         });
     });
 });
